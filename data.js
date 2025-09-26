8f70dbb245c5917e3eb0857aefedcac81786b493
猪皮 <975825750@qq.com>
2025-09-26 18:08:59 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,76 @@
+
+const allGames = [
+    { 
+        id: 1, 
+        name: "星际探险", 
+        type: "mobile", 
+        isFeatured: true,
+        icon: "./images/icons/1.png",
+        mainImage: "./images/icons/1.png",
+        qrCode: "./images/qrcodes/1.png",
+        screenshots: [
+            "./images/screenshots/1/1.png",
+            "./images/screenshots/1/2.png",
+            "./images/screenshots/1/3.png"
+        ],
+        description: "一款太空探索类角色扮演游戏",
+        size: "1.2GB",
+        version: "1.3.2",
+        updateDate: "2025-09-30"
+    },
+    { 
+        id: 2, 
+        name: "赛车传奇", 
+        type: "mobile", 
+        isFeatured: true,
+        icon: "./images/icons/2.png",
+        mainImage: "./images/icons/2.png",
+        qrCode: "./images/qrcodes/2.png",
+        screenshots: [
+            "./images/screenshots/2/1.png",
+            "./images/screenshots/2/2.png",
+            "./images/screenshots/2/3.png"
+        ],
+        description: "极速赛车体验",
+        size: "850MB",
+        version: "2.1.0",
+        updateDate: "2023-09-28"
+    },
+    { 
+        id: 3, 
+        name: "王国守卫者", 
+        type: "pc", 
+        isFeatured: false,
+        icon: "./images/icons/3.png",
+        mainImage: "./images/icons/3.png",
+        qrCode: "./images/qrcodes/3.png",
+        screenshots: [
+            "./images/screenshots/3/1.png",
+            "./images/screenshots/3/2.png",
+            "./images/screenshots/3/3.png"
+        ],
+        description: "策略塔防游戏",
+        size: "3.5GB",
+        version: "4.2.1",
+        updateDate: "2023-10-12"
+    },
+    { 
+        id: 4, 
+        name: "恋爱物语", 
+        type: "gentleman", 
+        isFeatured: false,
+        icon: "./images/icons/4.png",
+        mainImage: "./images/icons/4.png",
+        qrCode: "./images/qrcodes/4.png",
+        screenshots: [
+            "./images/screenshots/4/1.png",
+            "./images/screenshots/4/2.png",
+            "./images/screenshots/4/3.png"
+        ],
+        description: "校园恋爱冒险游戏",
+        size: "2.8GB",
+        version: "2.3.4",
+        updateDate: "2023-09-30"
+    }
+];
+    
