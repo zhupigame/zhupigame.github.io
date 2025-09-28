e7c6b31515ead0b4d16d7c73e78cf32079a2db23
猪皮 <975825750@qq.com>
2025-09-28 17:31:52 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,10 +1,11 @@
 
 const allGames = [
+    // 手机游戏
     { 
         id: 1, 
         name: "星际探险", 
         type: "mobile", 
-        isFeatured: true,
+        isFeatured: true,  // 热门游戏
         icon: "./images/icons/1.png",
         mainImage: "./images/icons/1.png",
         qrCode: "./images/qrcodes/1.png",
@@ -13,7 +14,7 @@ const allGames = [
             "./images/screenshots/1/2.png",
             "./images/screenshots/1/3.png"
         ],
-        description: "一款太空探索类角色扮演游戏",
+        description: "一款太空探索类角色扮演游戏，玩家将驾驶自己的飞船探索未知星系，与外星文明接触，完成各种任务和挑战。",
         size: "1.2GB",
         version: "1.3.2",
         updateDate: "2025-09-30"
@@ -22,55 +23,91 @@ const allGames = [
         id: 2, 
         name: "赛车传奇", 
         type: "mobile", 
-        isFeatured: true,
+        isFeatured: true,  // 热门游戏
         icon: "./images/icons/2.png",
         mainImage: "./images/icons/2.png",
-        qrCode: "./images/qrcodes/2.png",
+        qrCode: "./images/qrcodes/1.png",
         screenshots: [
             "./images/screenshots/2/1.png",
             "./images/screenshots/2/2.png",
-            "./images/screenshots/2/3.png"
         ],
-        description: "极速赛车体验",
+        description: "极速赛车体验，多种赛道选择，超过50种不同型号的赛车可供解锁和升级。",
         size: "850MB",
         version: "2.1.0",
         updateDate: "2023-09-28"
     },
     { 
         id: 3, 
-        name: "王国守卫者", 
-        type: "pc", 
+        name: "农场物语", 
+        type: "mobile", 
         isFeatured: false,
         icon: "./images/icons/3.png",
         mainImage: "./images/icons/3.png",
-        qrCode: "./images/qrcodes/3.png",
+        qrCode: "./images/qrcodes/1.png",
         screenshots: [
             "./images/screenshots/3/1.png",
-            "./images/screenshots/3/2.png",
-            "./images/screenshots/3/3.png"
+            "./images/screenshots/3/2.png"
         ],
-        description: "策略塔防游戏",
+        description: "模拟经营类游戏，玩家可以种植作物、养殖动物，打造自己的梦幻农场。",
+        size: "560MB",
+        version: "3.0.5",
+        updateDate: "2023-10-05"
+    },
+    
+    // 电脑游戏
+    { 
+        id: 101, 
+        name: "王国守卫者", 
+        type: "pc", 
+        isFeatured: true,  // 热门游戏
+        icon: "./images/icons/101.png",
+        mainImage: "./images/icons/101.png",
+        qrCode: "./images/qrcodes/1.png",
+        screenshots: [
+            "./images/screenshots/101/1.png",
+            "./images/screenshots/101/2.png",
+        ],
+        description: "策略塔防游戏，玩家需要建造各种防御塔，布置防御工事，抵御敌人的进攻。",
         size: "3.5GB",
         version: "4.2.1",
         updateDate: "2023-10-12"
     },
     { 
-        id: 4, 
+        id: 102, 
+        name: "机甲战士", 
+        type: "pc", 
+        isFeatured: true,
+        icon: "./images/icons/102.png",
+        mainImage: "./images/icons/102.png",
+        qrCode: "./images/qrcodes/1.png",
+        screenshots: [
+            "./images/screenshots/102/1.png",
+            "./images/screenshots/102/2.png"
+        ],
+        description: "科幻题材动作游戏，操控巨型机甲进行战斗，自定义机甲装备和武器。",
+        size: "8.2GB",
+        version: "1.1.0",
+        updateDate: "2023-09-20"
+    },
+    
+    // 绅士游戏
+    { 
+        id: 201, 
         name: "恋爱物语", 
         type: "gentleman", 
-        isFeatured: false,
-        icon: "./images/icons/4.png",
-        mainImage: "./images/icons/4.png",
-        qrCode: "./images/qrcodes/4.png",
+        isFeatured: true,
+        icon: "./images/icons/201.png",
+        mainImage: "./images/icons/201.png",
+        qrCode: "./images/qrcodes/1.png",
         screenshots: [
-            "./images/screenshots/4/1.png",
-            "./images/screenshots/4/2.png",
-            "./images/screenshots/4/3.png"
+            "./images/screenshots/201/1.png",
+            "./images/screenshots/201/2.png",
         ],
-        description: "校园恋爱冒险游戏",
+        description: "校园恋爱冒险游戏，玩家将扮演转学生，在新的学校中认识各种性格各异的角色。",
         size: "2.8GB",
         version: "2.3.4",
         updateDate: "2023-09-30"
-    }
+    },
+
 ];
     
