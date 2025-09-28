26eef18f32f031a86392e0f2d2516f05d0ccce41
猪皮 <975825750@qq.com>
2025-09-28 21:15:43 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -3,35 +3,50 @@ const allGames = [
     // 手机游戏
     { 
         id: 1, 
-        name: "星际探险", 
+        name: "时间静止-重返校园", 
         type: "mobile", 
         isFeatured: true,  // 热门游戏
         icon: "./images/icons/1.png",
         mainImage: "./images/icons/1.png",
-        qrCode: "./images/qrcodes/1.png",
+        qrCode: "./images/qrcodes/1.jpg",
         screenshots: [
-            "./images/screenshots/1/1.png",
-            "./images/screenshots/1/2.png",
-            "./images/screenshots/1/3.png"
+            "./images/screenshots/1/1.jpg",
+            "./images/screenshots/1/2.jpg",
         ],
-        description: "一款太空探索类角色扮演游戏，玩家将驾驶自己的飞船探索未知星系，与外星文明接触，完成各种任务和挑战。",
-        size: "1.2GB",
-        version: "1.3.2",
-        updateDate: "2025-09-30"
+    description: `重返校园中文手机版是一款校园生活模拟类型的游戏，玩家可以进入到一个由虚幻5引擎打造的真实校园场景之中，并在这里体验学生的生活，玩家可以得到丰富的游玩体验并可以探索更多的游戏场景，玩家可以得到不错的游玩体验。感兴趣的玩家就来这里进行下载和游玩吧!
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">1、抽象与正剧并存</span>
+        <br>
+        游戏的剧情风格奇特，在荒诞的情节中突然插入正剧元素，如舍友因家庭情况和主角影响走上歧途等，形成强烈的反差感，让玩家在游戏过程中不断感受到惊喜和意外。
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">2、丰富的小彩蛋</span>
+        <br>
+        在游戏的各个角落都隐藏着作者设置的小彩蛋，如爸爸的奇怪要求、厕所隔间的提示等，增加了游戏的趣味性和探索性。
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">3、内置小游戏</span>
+        <br>
+        玩家在游戏内的手机中可以玩到3D跳跃解密游戏和第一人称恐怖游戏等，丰富了游戏的内容和玩法。
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">4、隐藏剧情</span>
+        <br>
+        还有各种支线任务和隐藏任务，玩家需要通过与不同的角色互动、探索校园的各个角落来触发和完成任务。`,
+    size: "2030.89M",
+    version: "1.0",
+    updateDate: "2025-09-30"
     },
     { 
         id: 2, 
-        name: "赛车传奇", 
+        name: "白色情人节-恐怖学校", 
         type: "mobile", 
         isFeatured: true,  // 热门游戏
         icon: "./images/icons/2.png",
         mainImage: "./images/icons/2.png",
-        qrCode: "./images/qrcodes/1.png",
+       qrCode: "./images/qrcodes/1.jpg",
         screenshots: [
-            "./images/screenshots/2/1.png",
-            "./images/screenshots/2/2.png",
+            "./images/screenshots/2/1.jpg",
+            "./images/screenshots/2/2.jpg",
         ],
-        description: "极速赛车体验，多种赛道选择，超过50种不同型号的赛车可供解锁和升级。",
+        description: `恐怖学校白色情人节是一款非常恐怖的逃生类游戏。游戏采用第一视角亲身体验游戏里面的恐怖故事，配合黑暗的游戏画风加上恐怖的背景音乐，瞬间营造出一种惊悚的氛围，让感到一股寒气从尾椎骨往上冒，让人头皮发麻。`,
         size: "850MB",
         version: "2.1.0",
         updateDate: "2023-09-28"
@@ -43,7 +58,7 @@ const allGames = [
         isFeatured: false,
         icon: "./images/icons/3.png",
         mainImage: "./images/icons/3.png",
-        qrCode: "./images/qrcodes/1.png",
+       qrCode: "./images/qrcodes/1.jpg",
         screenshots: [
             "./images/screenshots/3/1.png",
             "./images/screenshots/3/2.png"
@@ -62,7 +77,7 @@ const allGames = [
         isFeatured: true,  // 热门游戏
         icon: "./images/icons/101.png",
         mainImage: "./images/icons/101.png",
-        qrCode: "./images/qrcodes/1.png",
+       qrCode: "./images/qrcodes/1.jpg",
         screenshots: [
             "./images/screenshots/101/1.png",
             "./images/screenshots/101/2.png",
@@ -79,7 +94,7 @@ const allGames = [
         isFeatured: true,
         icon: "./images/icons/102.png",
         mainImage: "./images/icons/102.png",
-        qrCode: "./images/qrcodes/1.png",
+       qrCode: "./images/qrcodes/1.jpg",
         screenshots: [
             "./images/screenshots/102/1.png",
             "./images/screenshots/102/2.png"
@@ -98,7 +113,7 @@ const allGames = [
         isFeatured: true,
         icon: "./images/icons/201.png",
         mainImage: "./images/icons/201.png",
-        qrCode: "./images/qrcodes/1.png",
+       qrCode: "./images/qrcodes/1.jpg",
         screenshots: [
             "./images/screenshots/201/1.png",
             "./images/screenshots/201/2.png",
