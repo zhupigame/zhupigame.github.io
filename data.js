036a806ca3f20149c0784ab8c30e36450b492d97
猪皮 <975825750@qq.com>
2025-09-29 21:42:57 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,17 +1,88 @@
 
 const allGames = [
-    // 手机游戏
-    { 
+        { 
         id: 1, 
-        name: "时间静止-重返校园", 
-        type: "mobile", 
-        isFeatured: true,  // 热门游戏
+        name: "塞雷卡2", 
+        type: ["mobile","pc","gentleman"], 
+		qrCode: "./images/qrcodes/1.jpg",
+        isFeatured: true,
         icon: "./images/icons/1.png",
-        mainImage: "./images/icons/1.png",
-        qrCode: "./images/qrcodes/1.jpg",
+        mainImage: "./images/icons/1.png",       
         screenshots: [
             "./images/screenshots/1/1.jpg",
-            "./images/screenshots/1/2.jpg",
+            "./images/screenshots/1/2.jpg"
+        ],
+    description: `《塞雷卡 2：真菜香的禁忌》作为一款备受瞩目的剧情向游戏，通过精巧的叙事手法，逐步引领玩家深入剧情。游戏承袭了前作的世界观，并深入探索了“真菜香”所蕴含的禁忌之谜。玩家需在探索过程中细心挖掘隐藏的线索，逐步揭开每个角色深藏的秘密!
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">1、沉浸式潜行体验</span>
+        <br>
+        置身于逼真的住宅区、购物中心等环境中，你将执行高风险任务，感受第一人称视角下的紧张刺激。动态天气与昼夜循环为每个角落增添了未知与变数，暴雨夜的阴影将成为你的最佳掩护，而正午的强光则可能暴露你的行踪。玩家实测反馈：“在天桥执行任务时，一个路人的发现带来的3D视野压迫感，简直像是在玩恐怖游戏！
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">2、暴露与伪装的艺术</span>
+        <br>
+        拥有超过50套可自定义服装与彩绘系统，你可以自由切换“隐形人”与“诱惑者”的双重身份。在商务场合中，你可能是西装革履的绅士，下一秒却可能露出彩绘纹身挑衅NPC。而内衣外穿的冒险者，则能通过精准走位在人群中巧妙脱身。创意玩法如“外套呼脸”“手铐束缚”等，将潜行游戏转变为一场充满黑色幽默的行为艺术盛宴。
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">3、策略性任务设计</span>
+        <br>
+        从“货车车头点位”的道具操作到无人机任务的极限挑战，每个任务都要求你因地制宜地制定战术。玩家分享：“我曾尝试用彩绘伪装成清洁工在购物中心执行任务，结果被巡逻AI识破，紧急躲进试衣间换装的那一刻，肾上腺素飙升的感觉简直比吃鸡决赛圈还要刺激！”
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">4、玩家体验</span>
+        <br>
+        值得一提的是，其针对低配置设备的优化使得中端机器也能轻松运行，从而让更多玩家能够享受到潜行的乐趣。在《塞雷卡 2》的开放世界中，道德与欲望的冲突无处不在，暴露与隐蔽的生存法则被演绎得淋漓尽致。这款游戏以极致的潜行体验，诠释了真正的冒险并非简单的硬碰硬对决，而是智慧与胆量的绝妙配合。`,
+		size: "2.1GB",
+        version: "1.1.1",
+        updateDate: "2025-09-28"
+    },
+	    { 
+        id: 2, 
+        name: "罗克珊", 
+        type: ["mobile","pc","gentleman"], 
+		qrCode: "./images/qrcodes/1.jpg",
+        isFeatured: true, 
+        icon: "./images/icons/2.png",
+        mainImage: "./images/icons/2.png",
+        screenshots: [
+            "./images/screenshots/2/1.jpg",
+            "./images/screenshots/2/2.jpg",
+        ],
+    description: `游戏主打自由探索和角色扮演，玩家要控制性格内向但有特殊爱好的女孩罗克珊，在虚拟城市里进行独特冒险。!
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">1、核心内容</span>
+        <br>
+        1.自由探索：有6张以上不同风格的地图（图书馆、泳池、超市等），支持360度无死角视角切换，玩家能自由控制角色在城市里移动。
+		<br>
+		2.任务系统：通过主线剧情和支线任务（比如"手铐任务"）拿经验值，解锁新内容。要注意避开NPC视线，不然可能触发游戏漏洞或者任务失败。
+		<br>
+		3.服装互动：有66套以上能换的服装（内衣、泳衣、配饰等），部分服装有动态效果（比如泳衣遇水溶解）。
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">2、暴露与伪装的艺术</span>
+        <br>
+        主角罗克珊表面腼腆害羞，心里却想通过"暴露行为"得到自由感。玩家要帮她在城市里实现这个隐秘欲望，同时应对NPC的监视和各种潜在风险。
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">3、游戏特色</span>
+        <br>
+        1.高自由度设计：除了主线，玩家能自己选探索区域、服装搭配和互动方式，还能用环境隐藏（比如躲进垃圾箱或长椅下面）。
+		<br>
+		2.智能NPC系统：NPC行为优化过，视野只在前面区域，不会僵着看玩家；部分NPC会随机逃跑或者触发特殊事件（比如泳池出现警车）。
+		<br>
+		3.细节优化：新增绳索去除功能（身体标记）、饮料补充能量、隐藏状态特殊互动等，让游戏更真实。
+		<br>
+		4.动态场景：支持360度旋转视角，能全方位看角色动作和环境细节，特别是淋浴区等特殊场景。`,
+		size: "2.1GB",
+        version: "0.6.2",
+        updateDate: "2025-09-28"
+    },
+	{ 
+        id: 3, 
+        name: "时间静止-重返校园", 
+        type: ["mobile","gentleman"], 
+		qrCode: "./images/qrcodes/1.jpg",
+        isFeatured: true,  
+        icon: "./images/icons/3.png",
+        mainImage: "./images/icons/3.png",       
+        screenshots: [
+            "./images/screenshots/3/1.jpg",
+            "./images/screenshots/3/2.jpg",
         ],
     description: `重返校园中文手机版是一款校园生活模拟类型的游戏，玩家可以进入到一个由虚幻5引擎打造的真实校园场景之中，并在这里体验学生的生活，玩家可以得到丰富的游玩体验并可以探索更多的游戏场景，玩家可以得到不错的游玩体验。感兴趣的玩家就来这里进行下载和游玩吧!
         <br><br>
@@ -35,94 +106,134 @@ const allGames = [
     updateDate: "2025-09-30"
     },
     { 
-        id: 2, 
+        id: 4, 
         name: "白色情人节-恐怖学校", 
-        type: "mobile", 
-        isFeatured: true,  // 热门游戏
-        icon: "./images/icons/2.png",
-        mainImage: "./images/icons/2.png",
-       qrCode: "./images/qrcodes/1.jpg",
+        type: ["mobile"], 
+		qrCode: "./images/qrcodes/1.jpg",
+        isFeatured: true,  
+        icon: "./images/icons/4.png",
+        mainImage: "./images/icons/4.png",
         screenshots: [
-            "./images/screenshots/2/1.jpg",
-            "./images/screenshots/2/2.jpg",
+            "./images/screenshots/4/1.jpg",
+            "./images/screenshots/4/2.jpg",
         ],
         description: `恐怖学校白色情人节是一款非常恐怖的逃生类游戏。游戏采用第一视角亲身体验游戏里面的恐怖故事，配合黑暗的游戏画风加上恐怖的背景音乐，瞬间营造出一种惊悚的氛围，让感到一股寒气从尾椎骨往上冒，让人头皮发麻。`,
         size: "850MB",
         version: "2.1.0",
-        updateDate: "2023-09-28"
+        updateDate: "2025-09-28"
     },
+   
     { 
-        id: 3, 
-        name: "农场物语", 
-        type: "mobile", 
-        isFeatured: false,
-        icon: "./images/icons/3.png",
-        mainImage: "./images/icons/3.png",
-       qrCode: "./images/qrcodes/1.jpg",
+        id: 5, 
+        name: "爬行实验室", 
+        type: ["mobile","pc","gentleman"], 
+		qrCode: "./images/qrcodes/1.jpg",
+        isFeatured: true,
+        icon: "./images/icons/5.png",
+        mainImage: "./images/icons/5.png", 
         screenshots: [
-            "./images/screenshots/3/1.png",
-            "./images/screenshots/3/2.png"
+            "./images/screenshots/5/1.jpg",
+            "./images/screenshots/5/2.jpg"
         ],
-        description: "模拟经营类游戏，玩家可以种植作物、养殖动物，打造自己的梦幻农场。",
-        size: "560MB",
-        version: "3.0.5",
-        updateDate: "2023-10-05"
+    description: `"爬行实验室"，是新纶工业在深山中建造的秘密设施。
+在这里，正在研究使用新开发的人工生命体"生物娃娃"的新产品。
+然而有一天，网络爬行人工智能窃取了第二代'生物娃娃'的身体，自称为“爬行女王”，并控制了实验室的所有设施。
+主角奉命前往实验室取回研究资料，却因一场意外事故，被独自留在实验室……他能否逃出爬行女王控制的实验室？
+<br>
+获取弹药并升级你的枪来击败爬行女王的爪牙。逃离实验室。!
+
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">1、核心内容</span>
+        <br>
+        1.自由探索：有6张以上不同风格的地图（图书馆、泳池、超市等），支持360度无死角视角切换，玩家能自由控制角色在城市里移动。
+		<br>
+		2.任务系统：通过主线剧情和支线任务（比如"手铐任务"）拿经验值，解锁新内容。要注意避开NPC视线，不然可能触发游戏漏洞或者任务失败。
+		<br>
+		3.服装互动：有66套以上能换的服装（内衣、泳衣、配饰等），部分服装有动态效果（比如泳衣遇水溶解）。
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">2、暴露与伪装的艺术</span>
+        <br>
+        主角罗克珊表面腼腆害羞，心里却想通过"暴露行为"得到自由感。玩家要帮她在城市里实现这个隐秘欲望，同时应对NPC的监视和各种潜在风险。
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">3、游戏特色</span>
+        <br>
+        1.高自由度设计：除了主线，玩家能自己选探索区域、服装搭配和互动方式，还能用环境隐藏（比如躲进垃圾箱或长椅下面）。
+		<br>
+		2.智能NPC系统：NPC行为优化过，视野只在前面区域，不会僵着看玩家；部分NPC会随机逃跑或者触发特殊事件（比如泳池出现警车）。
+		<br>
+		3.细节优化：新增绳索去除功能（身体标记）、饮料补充能量、隐藏状态特殊互动等，让游戏更真实。
+		<br>
+		4.动态场景：支持360度旋转视角，能全方位看角色动作和环境细节，特别是淋浴区等特殊场景。`,
+		size: "2.1GB",
+        version: "0.6.2",
+        updateDate: "2025-09-28"
     },
     
-    // 电脑游戏
     { 
-        id: 101, 
-        name: "王国守卫者", 
-        type: "pc", 
-        isFeatured: true,  // 热门游戏
-        icon: "./images/icons/101.png",
-        mainImage: "./images/icons/101.png",
-       qrCode: "./images/qrcodes/1.jpg",
-        screenshots: [
-            "./images/screenshots/101/1.png",
-            "./images/screenshots/101/2.png",
-        ],
-        description: "策略塔防游戏，玩家需要建造各种防御塔，布置防御工事，抵御敌人的进攻。",
-        size: "3.5GB",
-        version: "4.2.1",
-        updateDate: "2023-10-12"
-    },
-    { 
-        id: 102, 
-        name: "机甲战士", 
-        type: "pc", 
+        id: 6, 
+        name: "棘罪修女", 
+        type: ["mobile","pc","gentleman"], 
+		qrCode: "./images/qrcodes/1.jpg",
         isFeatured: true,
-        icon: "./images/icons/102.png",
-        mainImage: "./images/icons/102.png",
-       qrCode: "./images/qrcodes/1.jpg",
+        icon: "./images/icons/6.png",
+        mainImage: "./images/icons/6.png",   
         screenshots: [
-            "./images/screenshots/102/1.png",
-            "./images/screenshots/102/2.png"
+            "./images/screenshots/6/1.jpg",
+            "./images/screenshots/6/2.jpg"
         ],
-        description: "科幻题材动作游戏，操控巨型机甲进行战斗，自定义机甲装备和武器。",
-        size: "8.2GB",
-        version: "1.1.0",
-        updateDate: "2023-09-20"
+    description: `《棘罪修女》的故事背景设定在一个被未知力量摧毁的理想乡马格海姆。伊妮莎，一位逃离教堂的修女，在废墟中寻找遭遇灾难的原因，同时踏上了净化魔物横行的棘罪之地的旅程!
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">1、角色设定</span>
+        <br>
+        伊妮莎是游戏的主角，她拥有修长的美腿和美丽的外貌，身着引人注目的高叉泳衣般的修女服，搭配白丝（可换黑丝）裤袜和高跟鞋。在战斗中，她手持长枪（或伞枪）与怪物激烈对抗。
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">2、特殊机制</span>
+        <br>
+        魔镜：靠近会被禁锢，需冲刺挣脱。
+		<br>
+		铁处女：献祭生命可解锁掉落物品（金币、钥匙、道具），但有几率被强制吞入。
+		<br>
+		血藤蔓：需用火系或毒系伤害打破。
+		<br>
+		肉通道：内含双胞胎BUFF，可获取特殊能力。`,
+		size: "3.7GB",
+        version: "0.7.5",
+        updateDate: "2025-09-29"
     },
-    
-    // 绅士游戏
-    { 
-        id: 201, 
-        name: "恋爱物语", 
-        type: "gentleman", 
+	    { 
+        id: 7, 
+        name: "魔女的侵袭", 
+        type: ["mobile","pc","gentleman"], 
+		qrCode: "./images/qrcodes/1.jpg",
         isFeatured: true,
-        icon: "./images/icons/201.png",
-        mainImage: "./images/icons/201.png",
-       qrCode: "./images/qrcodes/1.jpg",
+        icon: "./images/icons/7.png",
+        mainImage: "./images/icons/7.png",   
         screenshots: [
-            "./images/screenshots/201/1.png",
-            "./images/screenshots/201/2.png",
+            "./images/screenshots/7/1.jpg",
+            "./images/screenshots/7/2.jpg"
         ],
-        description: "校园恋爱冒险游戏，玩家将扮演转学生，在新的学校中认识各种性格各异的角色。",
-        size: "2.8GB",
-        version: "2.3.4",
-        updateDate: "2023-09-30"
+    description: `《魔女的侵袭》是一款充满魔幻与冒险元素的3D动作闯关游戏。玩家将在游戏中扮演一名勇敢的英雄，与邪恶的魔女展开激烈对抗。游戏拥有逼真精美的画面和流畅的动画效果，带给玩家身临其境的体验。通过探索广阔又危险的世界，解开隐藏的谜题和危机，携带各种强大武器和技能，挑战各类恶魔和怪物，逐步提升自身能力和技巧。!
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">1、游戏特色</span>
+        <br>
+        1、精美画面和流畅动画：游戏拥有逼真的3D画面和流畅的动画效果，让玩家仿佛置身于游戏世界之中。
+		<br>
+        2、多样化的角色互动：玩家可以与多位魔女角色进行互动，解锁不同的剧情和CG，为游戏增添更多乐趣。
+		<br>
+		3、回合制战斗系统：游戏采用回合制战斗，玩家需要在回合中指挥角色进行攻击、防御等操作，战斗策略至关重要。
+        <br><br>
+        <span style="color:#3B82F6;font-weight:bold">2、游戏亮点</span>
+        <br>
+        1、战斗系统策略性高：玩家需要根据不同敌人的特点，合理运用角色技能和装备，制定战术，才能取得胜利。
+		<br>
+		2、剧情丰富多变：游戏设置了丰富的剧情线索和角色互动，通过与魔女互动解锁不同的剧情，让玩家沉浸于游戏世界中。
+		<br>
+		3、全3D动态场景：游戏将玩家带入全3D动态场景中，细致精美的画面设计让游戏更加生动逼真。
+		<br>
+		4、丰富的角色和道具：游戏中设有多种角色和道具可供玩家选择和使用，增加游戏可玩性。`,
+		size: "3.7GB",
+        version: "0.7.5",
+        updateDate: "2025-09-29"
     },
 
 ];
-    
